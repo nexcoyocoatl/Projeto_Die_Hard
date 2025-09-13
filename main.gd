354028b4fc5c8c487b64fec7c587dcee412b2623
nexcoyocoatl <jukemaia@gmail.com>
2025-09-12 22:28:17 -0300

    adicionada função de tempo por unidades/action points e pausa do processamento

--- main.gd
+++ main.gd
@@ -1,40 +1,66 @@
 extends Node2D
 
-@export var tile_size : int = 128		# Tamanho de tile que os personagens acessam em seu script (provavelmente mudaremos depois)
-@export var pause_time : bool = true	# (WIP) Para pausar o jogo
+@export var tile_size : int = 128			# Tamanho de tile que os personagens acessam em seu script (provavelmente mudaremos depois)
+@export var pause_time : bool = true		# (WIP) Para pausar o jogo
+@export var move_cooldown : float = 0.3		# Cooldown para cada movimento (0.3 segundos para cada nova ação)
 
+var input_direction : Vector2						# Direção de movimento do jogador
+var move_cooldown_timer : float = 0.0		# Timer para realizar o cooldown de movimento
+var awaiting_done_confirmation = 0
 var action_points : int = 0
-# Cada ação do jogador, adiciona um action point
-# Cada action point, faz os NPCs realizaram uma ação
-
-# FUNÇÃO DE PAUSE ESTÁ EM WIP!
+var player_action_queue = []
+var moving : bool = false
 
 func _ready() -> void:
-	pause_time = true # (WIP) Pausa o jogo no início e a cada ação do jogador, para imitar o Nethack
-	pause_processing()
-	
+	moving = false
+	pause_processing() # Pausa o jogo no início e a cada ação do jogador, para imitar o Nethack
+
+# Ativa movimentos pelo input, que funciona por eventos de botões pressionados
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed:
-			action_points += 1
-			pause_time = false
-			resume_processing() # Despausa em cada botão pressionado
-			move_world()
+			if (pause_time):
+				resume_processing() # Despausa em cada botão pressionado, caso necessário
+			
+			# Direção do movimento
+			if(move_cooldown_timer <= 0):	# Só funciona quando acabar o cooldown
+				action_points += 1
+				
+				input_direction = Vector2.ZERO
+				if Input.is_action_pressed("player_move_up"):
+					input_direction = Vector2(0,-1)
+				elif Input.is_action_pressed("player_move_down"):
+					input_direction = Vector2(0,1)
+				elif Input.is_action_pressed("player_move_left"):
+					input_direction = Vector2(-1,0)
+				elif Input.is_action_pressed("player_move_right"):
+					input_direction = Vector2(1,0)
+						
+				if (input_direction != Vector2.ZERO):
+					player_action_queue.push_back(input_direction)
 
-# Ainda não utilizado
-func _physics_process(_delta) -> void:
+# Utilizado para movimentação também
+# (física roda diferente e de forma mais consistente que process, utilizar quando utilizar delta)
+func _physics_process(delta) -> void:
+	if move_cooldown_timer > 0:
+		move_cooldown_timer -= 1*delta
+		
+	if (action_points > 0 and moving == false):
+		moving = true
+		action_points -= 1
+		move_cooldown_timer = move_cooldown
+		move_world()
+	
+	if (awaiting_done_confirmation <= 0 and moving == true):
+		moving = false
+		print(awaiting_done_confirmation)
+		stop_world()
+
+# Nodo Game está setado pra nunca pausar e sempre rodará esta função (e de física) em loop
+func _process(delta) -> void:
 	pass
 
-# Nodo Main está setado pra nunca pausar e sempre rodará esta função em loop
-func _process(_delta) -> void:
-	pass
-	# Pausa se o flag estiver setado
-	#if (pause_time):
-		#OS.low_processor_usage_mode = true	# baixa o uso de CPU
-		#get_tree().paused = true			# pausa o jogo
-	#move_world()
-
-# Não sei se é viável
+# Pausa todos outros nodos
 func pause_processing():
 	pause_time = true
 	print("time paused")
@@ -48,15 +74,19 @@ func resume_processing():
 	OS.low_processor_usage_mode = false
 	get_tree().paused = false
 
+# Recebe confirmação dos nodos Movable (individualmente) quando pararem
+func child_done_confirmation() -> void:
+	awaiting_done_confirmation -= 1
+
+# Função para parar todos movimentos (é chamada quando recebe confirmação de todos filhos que pararam as ações)
+func stop_world():
+	# Fazer algo mais?
+	pause_processing()
+
+# Função para chamar todos filhos Movable para executarem um movimento
 func move_world():
-	# Enviar action points? Ou gerenciar todos os personagens?
-	while(action_points > 0):
-		action_points -= 1
-		#await get_tree().create_timer(1).timeout
-	
-	# Problema do pause_processing é aqui:
-	# Quando pausa? Precisaria esperar todos terminarem o movimento
-	#pause_processing()
-	
-	# Chamar cada jogador/NPC?
-	# Pausar depois de rodar uma vez?
+	awaiting_done_confirmation = get_tree().get_nodes_in_group("Movable").size()
+	$Player.receive_action(player_action_queue.pop_front())
+	for movable in get_tree().get_nodes_in_group("Movable"):
+		# Mover outros objetos/personagens
+		pass
--- player.gd
+++ player.gd
@@ -1,49 +1,45 @@
 extends CharacterBody2D
 
 @export var tween_speed : float = 0.2		# Velocidade da animação de translação (maior é mais devagar)
-@export var move_cooldown : float = 0.3		# Cooldown para cada movimento (0.3 segundos para cada nova ação)
 
 var tile_size : int = 0						# Inicializa em 0, mas recebe do Main ao iniciar o jogo
-var input_direction : Vector2						# Direção de movimento do jogador
+var input_direction : Vector2 = Vector2.ZERO				# Direção de movimento do jogador
 var moving : bool = false					# Se está movendo ou não
-var move_cooldown_timer : float = 0.0		# Timer para realizar o cooldown de movimento
+
+# Action queue e points do jogador
+var action_queue = []
+var action_points = 0
 
 func _ready() -> void:
 	tile_size = get_parent().tile_size		# Recebe do Main
 
 func _physics_process(delta: float) -> void:
-	# Cooldown timer para cada ação
-	if move_cooldown_timer > 0:
-		move_cooldown_timer -= 1*delta
-		#print("Cooldown timer: ", move_cooldown_timer)
+	# Se não está movendo e tem action points para usar, executa um por um
+	if (moving == false and action_points):
+		moving = true
+		input_direction = action_queue.pop_front()
+		move()
+
+func receive_action(action):
+	print("player received action")
+	action_points += 1
 	
-	# Direção do movimento
-	input_direction = Vector2.ZERO
-	if Input.is_action_pressed("player_move_up"):
-		input_direction = Vector2(0,-1)
-		move()
-	elif Input.is_action_pressed("player_move_down"):
-		input_direction = Vector2(0,1)
-		move()
-	elif Input.is_action_pressed("player_move_left"):
-		input_direction = Vector2(-1,0)
-		move()
-	elif Input.is_action_pressed("player_move_right"):
-		input_direction = Vector2(1,0)
-		move()
+	# Por enquanto só pra movimento, se usar outra ação ou botão, quebra a execução
+	action_queue.push_back(action)
 
 # Movimenta o jogador
 func move():
-	if input_direction:
-		if moving == false and move_cooldown_timer <= 0:
-			moving = true
-			var tween = create_tween()
-			tween.tween_property(self, "position", position + (input_direction * tile_size), tween_speed)
-			tween.tween_callback(move_false)
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + (input_direction * tile_size), tween_speed)
+	tween.tween_callback(move_false)
 
 # Função que desativa o movimento após uma ação
-# Como também liga o cooldown
-# (chamará o pause_time no futuro?)
 func move_false():
 	moving = false
-	move_cooldown_timer = move_cooldown
+	print("Move false")
+	
+	# Diminui os action points, e se acaba todos, avisa a cena Game (Main) que terminou
+	action_points -= 1
+	if action_points <= 0:
+		print("player stops moving")
+		get_tree().call_group("Game", "child_done_confirmation")
