464294a588f0726f2478b2ceaabb76a46ca241d1
Enrico Zini <enricoassmannzini@gmail.com>
2025-10-19 00:41:47 -0300

    (Naiumy) cena de game over

---
+++ game_over.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready():
+	pass
+	
+func _process(_delta):
+	pass
+
+func _on_restart_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
