1e0afd3a0010adb7a01897aad53d24e70f461410
nexcoyocoatl <jukemaia@gmail.com>
2025-11-08 04:06:32 -0300

    adicionada lógica temporária para cone de visão ignorar checkpoints

--- Characters/npc.gd
+++ Characters/npc.gd
@@ -161,8 +161,8 @@ func create_cone():
 		if (cone_ray.is_colliding()):
 			var colliding_object = cone_ray.get_collider()
 			
-			# TODO: Temporário pra evitar o LevelEnd como obstáculo de visão
-			if (colliding_object.name.contains("LevelEnd")):
+			# TODO: Temporário pra evitar o LevelEnd e Checkpoint como obstáculo de visão
+			if (colliding_object.name.contains("LevelEnd") or colliding_object.name.contains("Checkpoint")):
 				cone_ray.add_exception(colliding_object)
 			
 			if (!player_found and colliding_object == player):
